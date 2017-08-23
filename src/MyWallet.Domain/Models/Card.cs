2b8b59f51e34c99570aa32973e22144809d113cf
Samuel Diogo <samudiogo@gmail.com>
2017-08-23 02:14:01 -0300

    creating the generic entity class and Card domain model #2

---
+++ src/MyWallet.Domain/Models/Card.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Cryptography;
+using MyWallet.Domain.Models.Core;
+using static System.Text.Encoding;
+using Convert = System.Convert;
+
+namespace MyWallet.Domain.Models
+{
+    public class Card: Entity
+    {
+        public Card(Guid id, string number, DateTime dueDate, DateTime expirationDate, int cvv, decimal limit, bool isReleasingCreditAccepted)
+        {
+            Id = id;
+            Number = HashingCardNumber(number);
+            DueDate = dueDate;
+            ExpirationDate = expirationDate;
+            Cvv = cvv;
+            if(limit < 0) throw new Exception("The limit cannot be negative.");
+            Limit = limit;
+
+            _isReleasingCreditAccepted = isReleasingCreditAccepted;
+        }
+
+        public Card(){}
+        public string Number { get; }
+        public DateTime DueDate { get; }
+        public DateTime ExpirationDate { get; } 
+        public int Cvv { get; }
+        public decimal Limit { get; }
+
+        private readonly bool _isReleasingCreditAccepted;
+
+        public bool IsReleasingCreditAccepted()
+        {
+            return _isReleasingCreditAccepted;
+        }
+
+        
+
+        private static string HashingCardNumber( string cardNumber)
+        {
+            using (var sha = SHA512.Create())
+                return Convert.ToBase64String(sha.ComputeHash(UTF8.GetBytes(cardNumber)));
+        }
+
+
+
+    
+    }
+}
