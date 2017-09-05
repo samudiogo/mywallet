e55c7fa4922bb2e72ecaac077f6e8978491bc2a0
Samuel Diogo <samudiogo@gmail.com>
2017-09-05 16:51:59 -0300

    create a entity that represents one purchase

---
+++ src/MyWallet.Domain/Models/Purchase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MyWallet.Domain.Models.Core;
+
+namespace MyWallet.Domain.Models
+{
+    public class Purchase:Entity
+    {
+        public Purchase( string description, decimal amount):base(Guid.NewGuid())
+        {
+            Description = description;
+            Amount = amount;
+        }
+
+        protected Purchase() { }
+
+        public string Description { get; private set; }
+        public decimal Amount { get; private set; }
+    }
+}
