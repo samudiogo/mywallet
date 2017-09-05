62c218776dde778412e1d5298598888032745c6e
Samuel Diogo <samudiogo@gmail.com>
2017-09-05 16:53:05 -0300

    initial implementation of buy method

--- src/MyWallet.Domain/Models/Wallet.cs
+++ src/MyWallet.Domain/Models/Wallet.cs
@@ -11,7 +11,7 @@ namespace MyWallet.Domain.Models
         #region Events
         //The Observable pattern suits well when Wallet updates your card list and need to ajust automaticaly the realimit.
         //but, now (08/30/2017 04am) I have no idea about how to implement this pattern.
-        
+
         #endregion
 
         public Wallet(Guid id, User owner) : base(id)
@@ -28,8 +28,8 @@ namespace MyWallet.Domain.Models
 
         public void AjustRealtLimit(decimal newRealLimit)
         {
-            var newLimit = newRealLimit< 0 ? RealLimit + newRealLimit: newRealLimit;
-            if ( newLimit < 0) {RealLimit = 0m; return;}
+            var newLimit = newRealLimit < 0 ? RealLimit + newRealLimit : newRealLimit;
+            if (newLimit < 0) { RealLimit = 0m; return; }
 
             if (newLimit > GetMaximmumLimit()) throw new Exception("Your real limit can not to be bigger then the amount of your cards.");
 
@@ -38,16 +38,78 @@ namespace MyWallet.Domain.Models
 
         public void AddNewCard(Card newCard) => Cards.Add(newCard);
 
-
-        public void RemoveCardById(Guid cardId)
+        /// <summary>
+        /// This method should have only one responsability //TODO: Improve it
+        /// </summary>
+        /// <param name="cardId"></param>
+        public void RemoveCardByIdAndUpdateLimit(Guid cardId)
         {
             var card = Cards.FirstOrDefault(c => c.Id.Equals(cardId));
             if (card == null) throw new Exception("There is no card registered with this identifier.");
-            if(Cards.Remove(card)) AjustRealtLimit(-card.Limit);
+            if (Cards.Remove(card)) AjustRealtLimit(-card.Limit);
 
         }
 
         public decimal GetMaximmumLimit() => Cards.Sum(c => c.Limit);
+
+        #region purchase
+
+        public void Buy(Purchase purchase)
+        {
+            //check if amount is over the realimit
+            if (purchase.Amount > RealLimit) throw new Exception("Amount over RealLimit");
+
+            //get cards lists orderning by last due date and minimum limit
+            var idealCardList = Cards.OrderByDescending(c => c.DueDate)
+                                 .ThenBy(c => c.Limit);
+            Card idealCard;
+            var cardsSameDue = new List<Card>();
+            if (idealCardList.Count() == 1)
+                idealCard = idealCardList.First();
+            else
+            {
+
+                for (var i = 0; i < idealCardList.Count(); i++)
+                {
+                    var card = idealCardList.ElementAtOrDefault(i);
+                    var next = idealCardList.ElementAtOrDefault(i + 1);
+                    if ((card != null && next != null) && card.IsSameDueDate(next))
+                    {
+                        cardsSameDue.Add(card);
+                        cardsSameDue.Add(next);
+                    }
+                }
+                idealCard = cardsSameDue.OrderBy(c => c.Limit).First();
+            }
+            if (idealCard.IsPurchaseFitsLimit(purchase.Amount))
+                idealCard.RegisterPurchase(purchase);
+            else if (purchase.Amount <= GetMaximmumLimit())
+            {
+                var firtstPortion = new Purchase(purchase.Description, purchase.Amount - idealCard.Limit);
+                idealCard.RegisterPurchase(firtstPortion);
+
+                var remainAmount = (int)(purchase.Amount - firtstPortion.Amount) / cardsSameDue.Count;
+
+                //if wallet have another same due card, te
+                var remainCards = cardsSameDue.Any() ?
+                    cardsSameDue.Where(c => c.Id != idealCard.Id) :
+                    idealCardList.Where(c => c.Id != idealCard.Id);
+                
+                var nextCard = remainCards
+                    .OrderByDescending(c => c.DueDate)//Assert the right order
+                    .ThenBy(c => c.Limit)// TODO: transform it on extension method
+                    .First();
+
+                if (nextCard.IsPurchaseFitsLimit(remainAmount))
+                    nextCard.RegisterPurchase(new Purchase(purchase.Description, remainAmount));
+                
+                //TODO: refact this method (turn it recursive)
+
+            }
+        }
+
+        #endregion
+
 
         //TODO: implment C# events to update realLimit
     }
