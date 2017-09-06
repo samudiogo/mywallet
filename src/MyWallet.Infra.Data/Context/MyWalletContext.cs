f5a0f1e52adedd92b54423736414837e9352cfae
Samuel Diogo <samudiogo@gmail.com>
2017-09-06 11:43:25 -0300

    initial user ef mapping

---
+++ src/MyWallet.Infra.Data/Context/MyWalletContext.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MyWallet.Domain.Models;
+
+namespace MyWallet.Infra.Data.Context
+{
+    public class MyWalletContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Acquisition> Acquisitions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TODO: ADD Mapping Class
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var cfg = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(cfg.GetConnectionString("MyWalletContext"));
+        }
+    }
+    
+}
---
+++ src/MyWallet.Infra.Data/Mappings/UserMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyWallet.Domain.Models;
+
+namespace MyWallet.Infra.Data.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Id).ValueGeneratedOnAdd();
+            builder.Property(c => c.Email).IsRequired();
+            builder.Property(u => u.Name).IsRequired();
+            builder.Property(u => u.Token);
+        }
+    }
+}
