bb20202e087e7137cf575d3e5837cd4d497649d3
Samuel Diogo <samudiogo@gmail.com>
2017-09-06 03:31:43 -0300

    intial implementation of domain service

---
+++ src/MyWallet.Domain/Services/Contracts/ICardMonitor.cs
@@ -0,0 +1,11 @@
+﻿using MyWallet.Domain.Models;
+
+namespace MyWallet.Domain.Services.Contracts
+{
+    public interface ICardMonitor
+    {
+        void DecreaseLimit(ref Card creditCard, decimal amountValue);
+        void IncreaseLimit(ref Card creditCard, decimal amountValue);
+
+    }
+}
---
+++ src/MyWallet.Domain/Services/Contracts/IPurchase.cs
@@ -0,0 +1,10 @@
+﻿using MyWallet.Domain.Models;
+
+namespace MyWallet.Domain.Services.Contracts
+{
+    public interface IPurchase
+    {
+        Wallet Wallet { get;  }
+        void Buy(Acquisition acquisition);
+    }
+}
