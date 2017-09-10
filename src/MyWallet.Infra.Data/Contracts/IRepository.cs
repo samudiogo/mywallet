7aa72e492ed48b1c8fb79a33751700856c9332c7
Samuel Diogo <samudiogo@gmail.com>
2017-09-10 12:49:44 -0300

    base implementation of repository pattern

---
+++ src/MyWallet.Infra.Data/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace MyWallet.Infra.Data.Contracts
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        void Add(TEntity obj);
+        TEntity GetById(Guid id);
+        void Update(TEntity obj);
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+    }
+}
+}
---
+++ src/MyWallet.Infra.Data/Repository/Shared/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using MyWallet.Infra.Data.Context;
+using MyWallet.Infra.Data.Contracts;
+
+namespace MyWallet.Infra.Data.Repository.Shared
+{
+    public abstract class Repository<TEntity>: IRepository<TEntity> where TEntity:class
+    {
+        protected MyWalletContext Context;
+        protected DbSet<TEntity> DbSet;
+
+        protected Repository( MyWalletContext context)
+        {
+            Context = context;
+            DbSet = Context.Set<TEntity>();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public void Add(TEntity obj)
+        {
+            DbSet.Add(obj);
+            Context.SaveChanges();
+        }
+
+        public TEntity GetById(Guid id) => DbSet.Find(id);
+
+        public void Update(TEntity obj)
+        {
+            Context.Entry(obj).State = EntityState.Modified;
+            Context.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate) => DbSet.AsNoTracking().Where(predicate);
+    }
+}
