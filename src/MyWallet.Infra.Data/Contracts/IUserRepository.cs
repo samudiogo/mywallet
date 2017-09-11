d18410192e9d2d6d8572f85ab7d80218782cefb6
Samuel Diogo <samudiogo@gmail.com>
2017-09-11 01:12:36 -0300

    adding specfic methods for user repository

--- src/MyWallet.Infra.Data/Contracts/IUserRepository.cs
+++ src/MyWallet.Infra.Data/Contracts/IUserRepository.cs
@@ -1,9 +1,12 @@
-﻿using MyWallet.Domain.Models;
+﻿using System;
+using System.Threading.Tasks;
+using MyWallet.Domain.Models;
 
 namespace MyWallet.Infra.Data.Contracts
 {
     public interface IUserRepository : IRepository<User>
     {
-
+        Task<User> FindByTokenIdAsync(string token, Guid id);
+        Task<User> FindByEmailAsync(string email);
     }
 }
