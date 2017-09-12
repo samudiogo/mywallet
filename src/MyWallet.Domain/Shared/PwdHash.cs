9303e9bca2ee0408b02319c6f79783a15510cc3f
Samuel Diogo <samudiogo@gmail.com>
2017-09-12 03:28:30 -0300

    shared utilities for domain entities

---
+++ src/MyWallet.Domain/Shared/PwdHash.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MyWallet.Domain.Shared
+{
+    public static class PwdHash
+    {
+        public static string Encode(this string password)
+        {
+            return Convert.ToBase64String(SHA512.Create()
+                .ComputeHash(Encoding.UTF8.GetBytes(password)));
+        }
+    }
+}
---
+++ src/MyWallet.Domain/Shared/TokenJwt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using Jose;
+namespace MyWallet.Domain.Shared
+{
+    public class TokenJwt
+    {
+        private static readonly byte[] SecretKey = 
+            SHA512.Create().ComputeHash(Encoding.UTF8.GetBytes(@"E7CEC5F400A1ED277FD3DE9C05FB2B271EF9FDF80B8856F69EB2388A37BC6EC5EE7D03960CA65F12F6194D9298A3E2433131F3423639DBF42D0C0FE08DFAC49D"));
+
+        public static string Generate(object obj) =>
+            JWT.Encode(BuildPayload(obj), SecretKey, JwsAlgorithm.HS512);
+
+        private static Dictionary<string,object> BuildPayload(object obj)
+        {
+            return new Dictionary<string, object>
+            {
+                {"sub", obj },
+                {"exp", DateTime.Now.AddMinutes(30) }
+            };
+        }
+    }
+}
