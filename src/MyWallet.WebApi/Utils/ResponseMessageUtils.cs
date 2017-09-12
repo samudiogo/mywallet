3d4365a0a0ecf62dfd7dca06fa41a606820be88c
Samuel Diogo <samudiogo@gmail.com>
2017-09-12 03:34:31 -0300

    helper for keep a pattern of http response message

---
+++ src/MyWallet.WebApi/Utils/ResponseMessageUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace MyWallet.WebApi.Utils
+{
+    public class ResponseMessageUtils
+    {
+        public static HttpResponseMessage CustomMessage(HttpRequestMessage request, HttpStatusCode statusCode, string message)
+        {
+            return request.CreateResponse(statusCode, new
+            {
+                statusCode,
+                mensagem = message
+            });
+        }
+    }
+}
