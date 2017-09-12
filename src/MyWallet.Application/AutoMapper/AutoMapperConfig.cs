ef1b67d4185fe3e944de8547d21b60e7f2300d70
Samuel Diogo <samudiogo@gmail.com>
2017-09-12 03:30:57 -0300

    added configurations to use  entity constructors

---
+++ src/MyWallet.Application/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace MyWallet.Application.AutoMapper
+{
+    public class AutoMapperConfig
+    {
+        public static MapperConfiguration RegisterMappings()
+        {
+            return new MapperConfiguration(c => c.AddProfile(new AutoMapperProfile()));
+        }
+    }
+}
---
+++ src/MyWallet.Application/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AutoMapper;
+using MyWallet.Application.Dto;
+using MyWallet.Domain.Models;
+
+namespace MyWallet.Application.AutoMapper
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(u => u.Password, opt => opt.Ignore());
+            CreateMap<UserRegistrationDto, User>()
+                .ConstructUsing(src => new User(src.Name, src.Email, src.Password))
+                .ForMember(u => u.Password, opt => opt.Ignore()); ;
+            CreateMap<UserLoginDto, User>()
+                .ConstructUsing(src => new User(src.Email, src.Password)).ForMember(u => u.Password, opt => opt.Ignore());
+            CreateMap<UserDto, User>();
+
+        }
+    }
+}
