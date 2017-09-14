bc27b3615091903e6d18c582fa1f1353937a9721
Samuel Diogo <samudiogo@gmail.com>
2017-09-14 05:11:21 -0300

    error report response most close to stone pagamentos error feedback (I saw at stone api doc)

---
+++ src/MyWallet.WebApi/Models/ErrorReport.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyWallet.WebApi.Models
+{
+    public class ErrorReport
+    {
+        public string Category { get; set; }
+        public ICollection<ErrorItem> ErrorItemCollection { get; set; }
+
+        public static ICollection<ErrorItem> GetExceptionList(int errorCode, Exception exception)
+        {
+            var exList = new List<ErrorItem>();
+            var hasInner = false;
+            var actualException = exception;
+            do
+            {
+                exList.Add(new  ErrorItem
+                {
+                    Description = actualException.Message,
+                    ErrorCode = errorCode
+                });
+
+                if (actualException.InnerException == null) continue;
+
+                hasInner = true;
+                actualException = actualException.InnerException;
+
+            } while (hasInner);
+            return exList;
+        }
+    }
+    public class ErrorItem
+    {
+        public string Description { get; set; }
+        public int ErrorCode { get; set; }
+    }
+
+}
